c5bb9a3f7f58ff1949c45edd4bc10609179d1af7
rapalliprasannareddy2022 <renu.ramarao@gmail.com>
2026-04-22 14:44:06 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashbordComponent,children:[
+    {path:'home',component:HomeComponent},
+    {path:'welcome',component:WelcomeComponent}
+  ]},
+  {path:'**',component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
